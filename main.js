d9c069970a1af9ce9316606018d54aee58f86a82
AnnaBerk <38033737+AnnaBerk@users.noreply.github.com>
2019-02-17 20:22:17 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var link = document.querySelector(".register-link");
+var popup = document.querySelector(".modal");
+var close = document.querySelector(".modal-close");
+
+link.addEventListener("click", function(evt){
+	evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt){
+	evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
